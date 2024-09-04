71a0890c312bc3b1b72cbccd0951dcc00ba5628d
dulce <dulce.mauricio@alumno.buap.mx>
2024-09-04 00:21:57 -0600

    Ejercicio 1: Validación de variables

---
+++ tecweb/practicas/p05/index.php
@@ -0,0 +1,10 @@
+<?php
+/* Ejercicio 1 */
+echo "La variable \$_myvar es válida porque comienza con un guion bajo y un carácter válido.\n";
+echo "La variable \$_7var es válida porque comienza con un guion bajo y un número.\n";
+echo "La variable myvar NO es válida porque no comienza con un signo de dólar.\n";
+echo "La variable \$myvar es válida porque comienza con un signo de dólar seguido de un nombre válido.\n";
+echo "La variable \$var7 es válida porque comienza con un signo de dólar seguido de un nombre válido.\n";
+echo "La variable \$_element1 es válida porque comienza con un guion bajo seguido de un nombre válido.\n";
+echo "La variable \$house*5 NO es válida porque contiene un carácter no permitido (*).\n";
+?>
