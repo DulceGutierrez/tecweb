a3875c559b39b057471a729a68f9b54ac3cf4945
dulce <dulce.mauricio@alumno.buap.mx>
2024-09-04 01:24:18 -0600

    Ejercicio 3: Evolución de tipos de variables

--- tecweb/practicas/p05/index.php
+++ tecweb/practicas/p05/index.php
@@ -11,7 +11,7 @@ echo "La variable \$house*5 NO es válida porque contiene un carácter no permit
 
 
 /* Ejercicio 2*/
-echo "Ejercicio 2<br>";
+echo "<br>Ejercicio 2<br>";
 /*Primeras asignaciones*/
 echo "<br> Primera asignación<br>";
 $a = "ManejadorSQL";
@@ -30,5 +30,25 @@ echo "Valor de \$c: $c<br>";
 
 echo "<br>¿Qué ocurrió?<br>";
 echo "Al asignar \$b = &\$a, \$b se convierte en una referencia a \$a.
-Por lo tanto, cualquier cambio en \$a se refleja en \$b, pero \$c mantiene el valor anterior porque era una referencia al valor antiguo de \$a.<br>";
+Por lo tanto, cualquier cambio en \$a se refleja en \$b y en \$c porque son referencias al valor de \$a.<br>";
+
+/*Ejercicio 3*/
+echo "<br>Ejercicio 3<br>";
+$a = "PHP5";
+$z[] = &$a;
+$b = "5a version de PHP";
+echo "Valor de \$a después de asignación: $a<br>";
+echo "Valor de \$z[0]: " . print_r($z, true) . "<br>";
+echo "Valor de \$b: $b<br>";
+$c = (int)$b * 10;
+echo "Valor de \$c: $c<br>";
+$a .= $b;
+$b *= $c;
+$z[0] = "MySQL";
+echo "Después de todas las asignaciones:<br>";
+echo "Valor de \$a: $a<br>";
+echo "Valor de \$b: $b<br>";
+echo "Valor de \$c: $c<br>";
+echo "Valor de \$z[0]: " . print_r($z, true) . "<br>";
+
 ?>
